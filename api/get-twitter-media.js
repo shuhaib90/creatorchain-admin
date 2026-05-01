334cb7ce0290c8a8fb449324898b1a06f889cbaa
shuhaib90 <shuhaib90@users.noreply.github.com>
2026-05-01 22:12:27 +0530

    fix: convert api to commonjs to resolve deployment failure

--- api/get-twitter-media.js
+++ api/get-twitter-media.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+const axios = require('axios');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { url } = req.query;
 
   if (!url) {
@@ -35,4 +35,4 @@ export default async function handler(req, res) {
     console.error('Scraper Error:', error.response?.data || error.message);
     res.status(500).json({ error: 'Failed to fetch X media' });
   }
-}
+};
