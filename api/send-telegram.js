32a8e0708c7037e05cbea65d7539ffc9a9e956f8
shuhaib90 <shuhaib90@users.noreply.github.com>
2026-05-03 20:35:29 +0530

    Fix: CORS policy and Webhook authentication update

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -5,7 +5,6 @@ const TG_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '8299168473:AAFEH6t0sKDE0ZlFf
 
 export default async (req, res) => {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
